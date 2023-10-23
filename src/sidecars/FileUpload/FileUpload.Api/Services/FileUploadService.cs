e7ee455a351fdcbe423a4af7569adc6e716fe390
prenaissance <alexandru.andries@isa.utm.md>
2023-10-23 23:02:56 +0300

    feat(file-upload): update protobuf and implementation

--- src/sidecars/FileUpload/FileUpload.Api/Services/FileUploadService.cs
+++ src/sidecars/FileUpload/FileUpload.Api/Services/FileUploadService.cs
@@ -1,6 +1,8 @@
+using System.Net.Mime;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Grpc.Core;
+using SixLabors.ImageSharp.Formats.Png;
 using SwapSquare.GrpcFileUpload;
 
 namespace SwapSquare.FileUpload.Services;
@@ -15,12 +17,16 @@ public class FileUploadService(
     {
         string fileName;
         string mimeType;
+        ResizeParameters? resizeParameters;
+        byte[] firstChunk;
 
         // set the values from the first stream chunk
         if (await requestStream.MoveNext())
         {
             fileName = requestStream.Current.Name;
             mimeType = requestStream.Current.MimeType;
+            resizeParameters = requestStream.Current.ResizeParameters;
+            firstChunk = requestStream.Current.Chunk.ToByteArray();
         }
         else
         {
@@ -39,6 +45,40 @@ public class FileUploadService(
             throw new RpcException(new Status(StatusCode.AlreadyExists, "File already exists"));
         }
 
-        //
+        // process the file stream
+        using var stream = new MemoryStream();
+        using var image = Image.Load(stream);
+        if (resizeParameters != null)
+        {
+            image.Mutate(x => x.Resize(
+                new ResizeOptions
+                {
+                    Size = new Size((int)resizeParameters.Width, (int)resizeParameters.Height),
+                    Mode = resizeParameters.KeepAspectRatio ? ResizeMode.Max : ResizeMode.Crop
+                }));
+        }
+        using var outStream = new MemoryStream();
+        image.Save(outStream, new PngEncoder());
+        var blobTask = blobClient.UploadAsync(
+            outStream,
+            new BlobHttpHeaders { ContentType = MediaTypeNames.Image.Png });
+
+        // process the rest of the stream
+        await stream.WriteAsync(firstChunk);
+        while (await requestStream.MoveNext())
+        {
+            var chunk = requestStream.Current.Chunk.ToByteArray();
+            await stream.WriteAsync(chunk);
+        }
+        stream.Close();
+
+        // wait for the blob to finish uploading
+        var blobResult = await blobTask;
+        return new FileUploadResponse
+        {
+            Url = blobClient.Uri.ToString(),
+            MimeType = MediaTypeNames.Image.Png
+        };
+
     }
 };
